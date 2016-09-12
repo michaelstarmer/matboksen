bd7d2c68821a3e3683ca76a59c27e1a8c1b9019a
michaelstarmer <michael.starmer@gmail.com>
2016-09-13 00:25:05 +0200

    Favicons

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -22,7 +22,7 @@
         <![endif]-->
 
         <!-- Favicon and touch icons -->
-        <link rel="shortcut icon" href="{{ URL::asset('includes/ico/favicon.png') }}">
+        <link rel="shortcut icon" href="{{ URL::asset('includes/ico/favicon2.png') }}">
         <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ URL::asset('includes/ico/apple-touch-icon-144-precomposed.png') }}">
         <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ URL::asset('includes/ico/apple-touch-icon-114-precomposed.png') }}">
         <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ URL::asset('includes/ico/apple-touch-icon-72-precomposed.png') }}">
